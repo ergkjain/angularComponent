16cc8b6f50e6d1d9f55e52b8a34cb1826a56dddd
Gajendra Jain <er.gkjain@gmail.com>
2023-05-23 17:35:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  selector: 'app-root',
+
+  templateUrl: './app.component.html',
+
+  styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'AngularListComp';
+
+  title = 'List';
+
+  listObjects : any[]= [];
+
+  listValue :{
+
+    name? : String,
+
+    index? : number
+
+  } = {}
+
+
+
+
+  public addItem(){
+
+    this.listObjects.push({
+
+      name : this.listValue.name,
+
+      index : this.listObjects.length + 1
+
+    });
+
+  }
+
+
+
+
+  public deleteItem(index: number){
+
+    for(let i =0 ;i<this.listObjects.length;i++){
+
+      if(this.listObjects[i].index === index){
+
+        this.listObjects.splice(index-1, 1);
+
+      }
+
+    }
+
+  }
+
 }
